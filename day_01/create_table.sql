04b392ea49a299a91a58e3e2f5a5774d9b624522
repo.demo <sudipsubedi629@gmail.com>
2026-04-25 10:53:50 +0545

    Remove day_01 folder

--- day_01/create_table.sql
+++
@@ -1,8 +0,0 @@
--- Create students table
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    city VARCHAR(50),
-    marks INT
-);
--- day_01/select_queries.sql
+++
@@ -1,9 +0,0 @@
--- Select all data
-SELECT * FROM students;
-
--- Select specific columns
-SELECT name, marks FROM students;
-
--- Filter data
-SELECT * FROM students
-WHERE marks > 80;
